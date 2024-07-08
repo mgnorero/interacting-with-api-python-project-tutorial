3e02441aabfdd4f01daaf17dce6b4f1c9e2f9942
mgnorero <127898316+mgnorero@users.noreply.github.com>
2024-07-08 20:15:52 +0000

    concectar con api

--- src/app.py
+++ src/app.py
@@ -2,6 +2,13 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret))
